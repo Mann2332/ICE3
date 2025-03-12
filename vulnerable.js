084c91b899494e81e5073339ec20d83712b64950
Mann2332 <mohitdodiya2332@gmail.com>
2025-03-12 13:54:08 -0400

    Create vulnerable.js

---
+++ vulnerable.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// ❌ No input validation - allows Command Injection!
+app.get('/unsafe', (req, res) => {
+    let userInput = req.query.cmd;
+    require('child_process').exec(userInput, (err, stdout, stderr) => {
+        if (err) {
+            res.send(Error: ${err});
+        } else {
+            res.send(Command Output: ${stdout});
+        }
+    });
+});
+
+// ❌ Hardcoded secret (Security Misconfiguration)
+const secretKey = "MySuperSecretPassword123";
+
+app.listen(port, () => {
+    console.log(Server running at http://localhost:${port});
+});
